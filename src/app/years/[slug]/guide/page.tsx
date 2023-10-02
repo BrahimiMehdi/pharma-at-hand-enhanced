d8cd97d94fe512fbc5f06c06f765266c5b8e5a8a
Brahimi Mehdi <brahimi.mehdi37@gmail.com>
2023-10-02 20:02:05 +0100

    fire deployment

--- src/app/years/[slug]/guide/page.tsx
+++ src/app/years/[slug]/guide/page.tsx
@@ -5,8 +5,6 @@ import { FolderOpen, ArrowRightCircle } from "lucide-react";
 import ArticleDetails from "@/components/Articles/ArticleDetails";
 export default async function Years({ params }: { params: { slug: string } }) {
   const { guide } = await getYear(params.slug);
- 
-  
   return (
     <main className="flex bg-background min-h-screen relative flex-col items-center justify-between px-12 p-24">
       {guide ? <ArticleDetails article={guide} /> : <h1>Something went wrong</h1>}

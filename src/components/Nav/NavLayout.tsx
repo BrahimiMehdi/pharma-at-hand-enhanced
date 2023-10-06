4a2cd7fa887c3c401dccb7335663643f1606fc42
Brahimi Mehdi <brahimi.mehdi37@gmail.com>
2023-10-06 22:45:52 +0100

    fixes

--- src/components/Nav/NavLayout.tsx
+++ src/components/Nav/NavLayout.tsx
@@ -1,34 +1,44 @@
-"use client"
+"use client";
 
 import { Nav } from "./Nav";
 import { Calculator, Menu, Users2 } from "lucide-react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "../ui/button";
-import { useParams, usePathname } from "next/navigation"
-import { CalendarCheck2, PenSquare,HardDrive, Home, Map } from "lucide-react";
-import { extractFirstSubpath } from "@/lib/utils"
+import { useParams, usePathname } from "next/navigation";
+import { CalendarCheck2, PenSquare, HardDrive, Home, Map } from "lucide-react";
+import gsap from "gsap";
+import { extractFirstSubpath } from "@/lib/utils";
 type Props = {
   children: React.ReactNode;
 };
 
 function NavLayout({ children }: Props) {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const pathName = usePathname();
   useEffect(() => {
-    setOpen(false)
-  }, [pathName])
-  const {slug} = useParams()
-  
-  const current = extractFirstSubpath(pathName)
-  const primaryLinks = ["/","/articles","/events","/clubs-and-ngo"]
-  const NavLinks = ()=>{
-    if(primaryLinks.includes(current)){
+    const ctx = gsap.context(() => {
+      const mm = gsap.matchMedia();
+      mm.add("(max-width:1024px)", () => {
+        setOpen(false);
+      });
+      
+    });
+    return () => {
+      ctx.revert();
+    };
+  }, [pathName]);
+  const { slug } = useParams();
+
+  const current = extractFirstSubpath(pathName);
+  const primaryLinks = ["/", "/articles", "/events", "/clubs-and-ngo"];
+  const NavLinks = () => {
+    if (primaryLinks.includes(current)) {
       return [
         {
           link: "/",
           icon: <Home strokeWidth={1.2} size={"18px"} className="mr-2" />,
           name: "Home",
-          isActive:true,
+          isActive: true,
         },
         {
           link: "/articles",
@@ -45,16 +55,14 @@ function NavLayout({ children }: Props) {
           icon: <Users2 strokeWidth={1.2} size={"20px"} className="mr-2" />,
           name: "Clubs & NGO's",
         },
-      ]
-    }
-    else{
-      
+      ];
+    } else {
       return [
         {
           link: "/",
           icon: <Home strokeWidth={1.2} size={"18px"} className="mr-2" />,
           name: "Home",
-          isActive:true,
+          isActive: true,
         },
         {
           link: `/years/${slug}`,
@@ -71,22 +79,23 @@ function NavLayout({ children }: Props) {
           icon: <Calculator strokeWidth={1.2} size={"18px"} className="mr-2" />,
           name: "Calcule",
         },
-      ]
+      ];
     }
-    
   };
-  
+
   return (
-    <div className={`flex relative transition-all duration-300 ease-in-out  ${open ? "lg:pl-80 pl-0" : "lg:pl-0 pl-0"}`}>
+    <div
+      className={`flex relative transition-all duration-300 ease-in-out  ${open ? "lg:pl-80 pl-0" : "lg:pl-0 pl-0"}`}
+    >
       <Nav setOpen={setOpen} open={open} slug={slug} links={NavLinks()} />
       <div className="w-full lg:border-l">
         <div className="w-full fixed top-0 z-[9] lg:px-12  left-0 pt-8   px-6 ">
-        <Button   onClick={()=>setOpen(!open)} size={"icon"}>
-              <Menu strokeWidth={1.4} size={"24px"} />
-            </Button>
+          <Button onClick={() => setOpen(!open)} size={"icon"}>
+            <Menu strokeWidth={1.4} size={"24px"} />
+          </Button>
         </div>
         {children}
-        </div>
+      </div>
     </div>
   );
 }

7a513d13d59982acb941681c33ff9d741baeadf7
Brahimi Mehdi <brahimi.mehdi37@gmail.com>
2023-10-10 16:11:31 +0100

    changes

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,22 +1,23 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { ThemeToggle } from "../ThemeToggle";
-import { Send, X } from "lucide-react";
-import { Button } from "../ui/button";
+import { Instagram, Send, X ,Mail} from "lucide-react";
+import { Button,buttonVariants } from "../ui/button";
 import NavLink from "./NavLink";
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
- links:NavLinksType;
- slug:string | string[];
- open:boolean;
- setOpen : React.Dispatch<boolean>
+  links: NavLinksType;
+  slug: string | string[];
+  open: boolean;
+  setOpen: React.Dispatch<boolean>;
 }
 
-export const Nav = ({ className,links,open ,setOpen,slug}: SidebarProps) => {
-
+export const Nav = ({ className, links, open, setOpen, slug }: SidebarProps) => {
   return (
     <nav
       className={cn(
-        `pb-12 justify-between transition-all duration-300 ease-in-out lg:shadow-none shadow-md z-10 flex flex-col fixed ${open ? "left-0" :"-left-full "} top-0 h-full w-full max-w-xl lg:max-w-xs bg-background`,
+        `pb-12 justify-between transition-all duration-300 ease-in-out lg:shadow-none shadow-md z-10 flex flex-col fixed ${
+          open ? "left-0" : "-left-full "
+        } top-0 h-full w-full max-w-xl lg:max-w-xs bg-background`,
         className
       )}
     >
@@ -24,12 +25,12 @@ export const Nav = ({ className,links,open ,setOpen,slug}: SidebarProps) => {
         <div className="px-3 py-2">
           <div className="w-full mb-2  items-center flex justify-between">
             <h2 className=" text-lg font-semibold tracking-tight">Pharma At Hand</h2>
-            <Button  aria-label="menu-close"  className="" onClick={()=>setOpen(!open)} size={"icon"}>
+            <Button aria-label="menu-close" className="" onClick={() => setOpen(!open)} size={"icon"}>
               <X strokeWidth={1.4} size={"24px"} />
             </Button>
           </div>
           <div className="space-y-1 mt-6">
-            {links.map((link,index)=>(
+            {links.map((link, index) => (
               <NavLink slug={slug} key={index} isActive={link.isActive} link={link.link}>
                 {link.icon}
                 <span>{link.name}</span>
@@ -38,18 +39,26 @@ export const Nav = ({ className,links,open ,setOpen,slug}: SidebarProps) => {
           </div>
         </div>
         <div className="px-3 py-2">
-        <h2 className=" text-lg font-semibold tracking-tight">Contact</h2>
+          <h2 className=" text-lg font-semibold tracking-tight">Contact</h2>
 
-        <div className="space-y-1 mt-6">
-              <NavLink slug={"/contact"}   link={"/contact"}>
-              <Send strokeWidth={1.2} size={"18px"} className="mr-2" />
-                <span>Contact</span>
-              </NavLink>
+          <div className="space-y-1 mt-6">
+            <NavLink slug={"/contact"} link={"/contact"}>
+              <Mail strokeWidth={1.2} size={"18px"} className="mr-2" />
+              <span>Contact</span>
+            </NavLink>
           </div>
+          
         </div>
-    
       </div>
-      <div className="px-3 flex justify-end  ">
+      <div className="px-3 flex justify-between items-center  ">
+      <div className="space-y-1 flex items-center justify-center gap-x-8">
+            <a className={buttonVariants({variant:"ghost",size:"icon"})} href="http://instagram.com" target="_blank" rel="noopener noreferrer">
+              <Instagram />
+            </a>
+            <a className={buttonVariants({variant:"ghost",size:"icon"})} href="http://instagram.com" target="_blank" rel="noopener noreferrer">
+              <Send />
+            </a>
+          </div>
         <ThemeToggle />
       </div>
     </nav>

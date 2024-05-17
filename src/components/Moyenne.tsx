1d9808fa3934748a8bcc8d6992618c5536f9b1f6
Brahimi Mehdi <brahimi.mehdi37@gmail.com>
2024-05-17 21:29:52 +0100

    changes

--- src/components/Moyenne.tsx
+++ src/components/Moyenne.tsx
@@ -8,7 +8,7 @@ function Moyenne({totalCoeff}: Props) {
     const {tableModules} = useMainContext()
     const moyenCoeff = tableModules.map((item)=>item.moycoeff)
     const totalMoyenneCoeff = moyenCoeff.reduce((a,b)=>a+b,0) || 0
-    const final = (totalMoyenneCoeff/totalCoeff).toFixed(2)
+    const final = (totalMoyenneCoeff/totalCoeff).toFixed(4)
   return (
     <div className="flex flex-col gap-y-8">
      <h2 className="text-2xl font-bold">Votre Moyenne : <span>{final}</span></h2>

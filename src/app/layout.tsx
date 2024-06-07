6fab539239c71cedec898f587443bdd1d23e06de
Brahimi Mehdi <brahimi.mehdi37@gmail.com>
2024-06-07 23:17:57 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,8 @@ import { Montserrat } from "next/font/google";
 import Script from "next/script";
 import { MainContextProvider } from "./context/MainContext";
 import "./globals.css";
-export const dynamic = "force-static"
 const montserrat = Montserrat({ subsets: ["cyrillic"] });
-
+export const revalidate = 86400
 export const metadata = constructeMetadata()
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
